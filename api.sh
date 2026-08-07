072cd4f6b3c5efc4d1d0bca8c74810ac1b780300
ashtonx24 <asharc789@gmail.com>
2026-08-07 21:54:06 +0530

    Support older curl versions

--- api.sh
+++ api.sh
@@ -5,20 +5,34 @@ api_call() {
     shift 2
     local api_url="${PVE_API_URL:-https://${PVE_HOST}:8006/api2/json}"
     local -a tls_args=()
+    local response_file http_code curl_status
 
     if [[ "${PVE_TLS_VERIFY:-1}" == '0' ]]; then
         tls_args+=(--insecure)
     fi
 
-    curl --silent --show-error --fail \
+    response_file=$(mktemp)
+    http_code=$(curl --silent --show-error \
         "${tls_args[@]}" \
         --connect-timeout "${API_CONNECT_TIMEOUT:-5}" \
         --max-time "${API_TIMEOUT:-15}" \
         --request "$method" \
-        --fail-with-body \
         --header "Authorization: PVEAPIToken=${PVE_TOKEN_ID}=${PVE_TOKEN_SECRET}" \
         "${api_url%/}/${endpoint#/}" \
-        "$@"
+        --output "$response_file" \
+        --write-out '%{http_code}' \
+        "$@")
+    curl_status=$?
+
+    cat "$response_file"
+    rm -f -- "$response_file"
+
+    if [[ "$curl_status" -ne 0 ]]; then
+        return "$curl_status"
+    fi
+    if [[ "$http_code" =~ ^[45][0-9][0-9]$ ]]; then
+        return 22
+    fi
 }
 
 api_vm_status() {
